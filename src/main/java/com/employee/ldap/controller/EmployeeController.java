8ebc019ccb805eb373a79345807d6e60cedfc671
debadatta10pati <46395564+debadatta10pati@users.noreply.github.com>
2022-07-08 08:27:56 +0530

    Update EmployeeController.java

--- src/main/java/com/employee/ldap/controller/EmployeeController.java
+++ src/main/java/com/employee/ldap/controller/EmployeeController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.employee.ldap.service.UserService;
 
 @RestController
-@RequestMapping("/employees")
 public class EmployeeController {
 
     private static final Logger log = LoggerFactory.getLogger(EmployeeController.class);
